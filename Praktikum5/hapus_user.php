34e0c9fd139f4902cc2299f2de3761bd8a38222d
Ridhan Aufa Mahfud <50816255+ridhnauf@users.noreply.github.com>
2021-12-09 22:28:16 +0700

    Add files via upload

---
+++ Praktikum5/hapus_user.php
@@ -0,0 +1,6 @@
+<?php
+include "../Praktikum3/koneksi.php";
+$sql="delete from users where id_user= '$_GET[id]'";
+mysqli_query($con, $sql);
+mysqli_close($conn);
+header('location:tampil_user.php');
