e3b69a839a09768cb135021731c6be8f568e16a7
Ridhan Aufa Mahfud <50816255+ridhnauf@users.noreply.github.com>
2021-12-16 21:41:22 +0700

    Add files via upload

---
+++ Praktikum9/mhs_xml.php
@@ -0,0 +1,18 @@
+<?php
+include "../Praktikum3/koneksi.php";
+header('Content-Type: text/xml');
+$query = "SELECT * FROM mahasiswa";
+$hasil = mysqli_query($con, $query);
+$jumField = mysqli_num_fields($hasil);
+echo "<?xml version='1.0'?>";
+echo "<data>";
+while ($data = mysqli_fetch_array($hasil)) {
+    echo "<mahasiswa>";
+    echo "<nim>", $data['nim'], "</nim>";
+    echo "<nama>", $data['namamhs'], "</nama>";
+    echo "<jkel>", $data['jkel'], "</jkel>";
+    echo "<alamat>", $data['alamat'], "</alamat>";
+    echo "<tgllhr>", $data['tgllhr'], "</tgllhr>";
+    echo "</mahasiswa>";
+}
+echo "</data>";
