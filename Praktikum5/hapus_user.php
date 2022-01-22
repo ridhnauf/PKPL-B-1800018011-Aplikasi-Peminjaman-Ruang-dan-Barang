f57d7b11796ae4be5d95b11433c9a32e772ff66f
Ridhan Aufa Mahfud <50816255+ridhnauf@users.noreply.github.com>
2022-01-22 14:30:28 +0700

    Delete Praktikum5 directory

--- Praktikum5/hapus_user.php
+++
@@ -1,6 +0,0 @@
-<?php
-include "../Praktikum3/koneksi.php";
-$sql="delete from users where id_user= '$_GET[id]'";
-mysqli_query($con, $sql);
-mysqli_close($conn);
-header('location:tampil_user.php');
--- Praktikum5/logout.php
+++
@@ -1,4 +0,0 @@
-<?php
-session_start();
-session_destroy();
-echo "Anda telah sukses keluar sistem <b>LOGOUT</b>";
